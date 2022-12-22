f6036bbb0198c3bff4b8fd6ddfdb72ff4a62a316
Ruslan Moussayev <ninja095@yahoo.com>
2022-12-22 11:14:55 +0100

    added file .eslintrc.json

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,8 @@
+import genDiff from "../src/gendiff.js";
+import { expect, test } from '@jest/globals';
+import path from 'path';
+import { readFileSync } from 'fs';
+
+test('we test genDiff function ', () => {
+
+})
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -18,8 +18,15 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('0.8.0')
     .action((filepath1, filepath2) => {
-        const fullPath1 = path.resolve(process.cwd(), filepath1);
-        const fullPath2 = path.resolve(process.cwd(), filepath2);
+        console.log("we build full path from", {
+            "processPath": process.cwd(),
+            "fixtureFolder": "__fixtures__",
+            fileName: filepath1
+        })
+        const badFullPath1 = process.cwd()+"/__fixtures__"+filepath1;
+        console.log("without path.resolve()", badFullPath1)
+        const fullPath1 = path.resolve(process.cwd(), "__fixtures__", filepath1);
+        const fullPath2 = path.resolve(process.cwd(), "__fixtures__",filepath2);
 
         try {
             const data1 = fs.readFileSync(fullPath1, 'utf8');
