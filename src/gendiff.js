a28841da5f484b60157fbf49c051625a7658b526
Ruslan Moussayev <ninja095@yahoo.com>
2022-12-22 18:46:59 +0100

    refactor

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,33 +1,33 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
 const genDiff = (file1, file2) => {
-    let result = '{\n'
-    const data1 = Object.keys(file1)
-    const data2 = Object.keys(file2)
-    let keys = _.concat(data1, data2)
-    keys = _.sortBy(_.uniq(keys))
-    for (const key of keys) {
-        if (!Object.hasOwn(file2, key)) {
-            result += `- ${key}: ${file1[key]}\n`
-        } else if (
-          Object.hasOwn(file1, key) &&
-          Object.hasOwn(file2, key) &&
-          file1[key] === file2[key]
-        ) {
-            result += `  ${key}: ${file1[key]}\n`
-        } else if (
-          Object.hasOwn(file1, key) &&
-          Object.hasOwn(file2, key) &&
-          file1[key] !== file2[key]
-        ) {
-            result += `- ${key}: ${file1[key]}\n`
-            result += `+ ${key}: ${file2[key]}\n`
-        } else {
-            result += `+ ${key}: ${file2[key]}\n`
-        }
-    }
-    result += '}'
-    return result
-}
+  let result = '{\n';
+  const data1 = Object.keys(file1);
+  const data2 = Object.keys(file2);
+  let keys = _.concat(data1, data2);
+  keys = _.sortBy(_.uniq(keys));
+  for (const key of keys) {
+      if (!Object.hasOwn(file2, key)) {
+          result += `- ${key}: ${file1[key]}\n`
+      } else if (
+        Object.hasOwn(file1, key) &&
+        Object.hasOwn(file2, key) &&
+        file1[key] === file2[key]
+      ) {
+          result += `  ${key}: ${file1[key]}\n`
+      } else if (
+        Object.hasOwn(file1, key) &&
+        Object.hasOwn(file2, key) &&
+        file1[key] !== file2[key]
+      ) {
+          result += `- ${key}: ${file1[key]}\n`
+          result += `+ ${key}: ${file2[key]}\n`
+      } else {
+          result += `+ ${key}: ${file2[key]}\n`
+      }
+  };
+  result += '}';
+  return result;
+};
 
-export default genDiff
+export default genDiff;
