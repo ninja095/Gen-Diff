8d89bdf55d07113ce6f445b9081f0f5a59d2405d
Ruslan Moussayev <ninja095@yahoo.com>
2022-12-22 14:54:12 +0100

    added test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
 test('we test genDiff function ', () => {
     // arrange
@@ -19,7 +19,7 @@ test('we test genDiff function ', () => {
     // const data2 = fs.readFileSync(fullPath2, 'utf8');
     const content1 = JSON.parse(readFile('file1.json'));
     const content2 = JSON.parse(readFile('file2.json'));
-    const expectedResult = fs.readFileSync(path.resolve(process.cwd(), "__fixtures__", 'stringJson.txt'), 'utf8');
+    const expectedResult = fs.readFileSync(path.resolve(process.cwd(), "__fixtures__", 'stringJson.txt'), 'utf8').trim();
 
     // act
     const compareRes = genDiff(content1, content2);
