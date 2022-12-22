148a09c2778cff987971056a961cb916c4407350
Ruslan Moussayev <ninja095@yahoo.com>
2022-12-22 19:01:06 +0100

    refactor

--- src/gendiff.js
+++ src/gendiff.js
@@ -10,22 +10,22 @@ const genDiff = (file1, file2) => {
     if (!Object.hasOwn(file2, key)) {
       result += `- ${key}: ${file1[key]}\n`;
     } else if (
-      Object.hasOwn(file1, key) &&
-      Object.hasOwn(file2, key) &&
-      file1[key] === file2[key]
+      Object.hasOwn(file1, key)
+      && Object.hasOwn(file2, key)
+      && file1[key] === file2[key]
     ) {
       result += `  ${key}: ${file1[key]}\n`;
     } else if (
-      Object.hasOwn(file1, key) &&
-      Object.hasOwn(file2, key) &&
-      file1[key] !== file2[key]
+      Object.hasOwn(file1, key)
+      && Object.hasOwn(file2, key)
+      && file1[key] !== file2[key]
     ) {
       result += `- ${key}: ${file1[key]}\n`;
       result += `+ ${key}: ${file2[key]}\n`;
     } else {
       result += `+ ${key}: ${file2[key]}\n`;
     }
-  };
+  }
   result += '}';
   return result;
 };
