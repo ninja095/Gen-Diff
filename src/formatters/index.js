9f4ac004af44b21dc903760601c96296687b1a42
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-02 18:02:04 +0100

    replacement if on switch

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,12 +2,15 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 
 export default (data, format) => {
-  if (format === 'stylish') {
-    return stylish(data);
-  } if (format === 'plain') {
-    return plain(data);
-  } if (format === 'json') {
-    return JSON.stringify(data);
+  switch (format) {
+    case 'stylish':
+      return stylish(data);
+    case 'plain':
+      return plain(data);
+    case 'json':
+      return JSON.stringify(data);
+    default:
+      throw new Error(`Unknown type of format: ${format}`);
   }
-  throw new Error(`Unknown type of format: ${format}`);
 };
+
