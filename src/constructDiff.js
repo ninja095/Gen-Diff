805bf13c08c27e133dd69a2742fcb79a2ec24800
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-02 17:37:34 +0100

    replacement if on switch

--- src/constructDiff.js
+++ src/constructDiff.js
@@ -1,27 +1,35 @@
 import _ from 'lodash';
 
-const buildDiff = (data1, data2) => {
+const constructDiff = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   const diff = keys.map((key) => {
     let result;
     const value1 = data1[key];
     const value2 = data2[key];
-    if (_.isObject(value1) && _.isObject(value2)) {
-      result = { key, children: buildDiff(value1, value2), type: 'nested' };
-    } else if (!Object.hasOwn(data1, key)) {
-      result = { key, value2, type: 'added' };
-    } else if (!Object.hasOwn(data2, key)) {
-      result = { key, value1, type: 'deleted' };
-    } else if (value1 !== value2) {
-      result = {
-        key, value1, value2, type: 'changed',
-      };
-    } else {
-      result = { key, value1, type: 'unchanged' };
+
+    switch (true) {
+      case _.isObject(value1) && _.isObject(value2):
+        result = { key, children: constructDiff(value1, value2), type: 'nested' };
+        break;
+      case !Object.hasOwn(data1, key):
+        result = { key, value2, type: 'added' };
+        break;
+      case !Object.hasOwn(data2, key):
+        result = { key, value1, type: 'deleted' };
+        break;
+      case value1 !== value2:
+        result = {
+          key, value1, value2, type: 'changed',
+        };
+        break;
+      default:
+        result = { key, value1, type: 'unchanged' };
     }
+
     return result;
   });
   return diff;
 };
 
-export default buildDiff;
+
+export default constructDiff;
