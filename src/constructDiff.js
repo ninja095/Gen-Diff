fb7c3f401b47eb3347c5218766bde06e83c608c0
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-02 17:53:18 +0100

    comparison through _.isEqual

--- src/constructDiff.js
+++ src/constructDiff.js
@@ -10,7 +10,7 @@ const constructDiff = (data1, data2) => {
         return { key, value2: data2[key], type: 'added' };
       case !Object.hasOwn(data2, key):
         return { key, value1: data1[key], type: 'deleted' };
-      case data1[key] !== data2[key]:
+      case _.isEqual(data1[key], data2[key]):
         return { key, value1: data1[key], value2: data2[key], type: 'changed' };
       default:
         return { key, value1: data1[key], type: 'unchanged' };
