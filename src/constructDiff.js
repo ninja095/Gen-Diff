ffd4b3a237c97ae677778e839da8a156dd7a97ac
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-02 17:45:35 +0100

    removal of variables

--- src/constructDiff.js
+++ src/constructDiff.js
@@ -2,34 +2,22 @@ import _ from 'lodash';
 
 const constructDiff = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-  const diff = keys.map((key) => {
-    let result;
-    const value1 = data1[key];
-    const value2 = data2[key];
-
+  return keys.map((key) => {
     switch (true) {
-      case _.isObject(value1) && _.isObject(value2):
-        result = { key, children: constructDiff(value1, value2), type: 'nested' };
-        break;
+      case _.isObject(data1[key]) && _.isObject(data2[key]):
+        return { key, children: constructDiff(data1[key], data2[key]), type: 'nested' };
       case !Object.hasOwn(data1, key):
-        result = { key, value2, type: 'added' };
-        break;
+        return { key, value2: data2[key], type: 'added' };
       case !Object.hasOwn(data2, key):
-        result = { key, value1, type: 'deleted' };
-        break;
-      case value1 !== value2:
-        result = {
-          key, value1, value2, type: 'changed',
-        };
-        break;
+        return { key, value1: data1[key], type: 'deleted' };
+      case data1[key] !== data2[key]:
+        return { key, value1: data1[key], value2: data2[key], type: 'changed' };
       default:
-        result = { key, value1, type: 'unchanged' };
+        return { key, value1: data1[key], type: 'unchanged' };
     }
-
-    return result;
   });
-  return diff;
 };
+
 
 
 export default constructDiff;
