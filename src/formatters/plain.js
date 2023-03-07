adf413231fdb45349443d0d3c954dde90446f921
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-07 14:54:25 +0100

    refactor plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,29 +8,32 @@ const stringify = (value) => {
   return value;
 };
 
-const generateDiffLines = (tree, path = '') => {
-  const lines = tree.map((node) => {
-    const keyPath = path === '' ? `${node.key}` : `${path}.${node.key}`;
+const plain = (tree) => {
+  const generateDiffLines = (tree, path = '') => {
+    const lines = tree.map((node) => {
+      const keyPath = path === '' ? `${node.key}` : `${path}.${node.key}`;
 
-    switch (node.type) {
-      case 'nested':
-        return generateDiffLines(node.children, keyPath);
-      case 'added':
-        return `Property '${keyPath}' was added with value: ${stringify(node.value2)}`;
-      case 'deleted':
-        return `Property '${keyPath}' was removed`;
-      case 'changed':
-        return `Property '${keyPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
-      case 'unchanged':
-        return null;
-      default:
-        throw new Error(`Unknown type of diff: ${node.type}`);
-    }
-  });
+      switch (node.type) {
+        case 'nested':
+          return generateDiffLines(node.children, keyPath);
+        case 'added':
+          return `Property '${keyPath}' was added with value: ${stringify(node.value2)}`;
+        case 'deleted':
+          return `Property '${keyPath}' was removed`;
+        case 'changed':
+          return `Property '${keyPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+        case 'unchanged':
+          return null;
+        default:
+          throw new Error(`Unknown type of diff: ${node.type}`);
+      }
+    });
 
-  return [...lines]
-    .filter(Boolean)
-    .join('\n');
-};
+    return [...lines]
+      .filter(Boolean)
+      .join('\n');
+  };
+  return generateDiffLines(tree)
+}
 
-export default (tree) => generateDiffLines(tree);
+export default plain;
