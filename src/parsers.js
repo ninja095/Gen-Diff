15dc04ad61f56a2c5bf7fac856e8b3d8fdc38a3c
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-09 17:39:47 +0100

    refactor parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,14 @@
 import yaml from 'js-yaml';
 
 export default (data, format) => {
-  if (format === 'json') {
-    return JSON.parse(data);
-  } if (format === 'yml' || format === 'yaml') {
-    return yaml.load(data);
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+      return yaml.load(data);
+      case 'yml':
+        return yaml.load(data);
+    default:
+      throw new Error(`Unknown type of format: ${format}`);
   }
-  throw new Error(`Unknown type of format: ${format}`);
 };
