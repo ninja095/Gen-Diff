7877024a76ebacbf565b9903497b1d010844c4e6
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-21 16:01:30 +0100

    refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,21 +8,16 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8').trim();
 
-const formatters = {
-  stylish: () => readFile('expected_stylish.txt'),
-  plain: () => readFile('expected_plain.txt'),
-  json: () => readFile('expected_json.txt'),
-};
-
 test.each([
-  ['file1.json', 'file2.json', 'stylish'],
-  ['file1.yml', 'file2.yml', 'stylish'],
-  ['file1.json', 'file2.json', 'plain'],
-  ['file1.yml', 'file2.yml', 'plain'],
-  ['file1.json', 'file2.json', 'json'],
-  ['file1.yml', 'file2.yml', 'json'],
-])('genDiff-tests(%#)', (file1, file2, format = 'stylish') => {
-  const actual = genDiff(getFixturePath(file1), getFixturePath(file2), format);
-  const expected = formatters[format];
-  expect(actual).toBe(expected());
+  ['file1.json', 'file2.json', 'stylish', 'expected_stylish.txt'],
+  ['file1.yml', 'file2.yml', 'stylish', 'expected_stylish.txt'],
+  ['file1.json', 'file2.json', 'plain', 'expected_plain.txt'],
+  ['file1.yml', 'file2.yml', 'plain', 'expected_plain.txt'],
+  ['file1.json', 'file2.json', 'json', 'expected_json.txt'],
+  ['file1.yml', 'file2.yml', 'json','expected_json.txt'],
+])('genDiff-tests(%#)', (file1, file2, format, expectedFile) => {
+    const actual = genDiff(getFixturePath(file1), getFixturePath(file2), format);
+    const expected = readFile(expectedFile); expect(actual).toBe(expected);
 });
+
+
