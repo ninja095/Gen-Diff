1040ab2e4166af464d9263d0d8a05c2712fc5211
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-23 09:49:35 +0100

    refactor file parsers

--- src/parsers.js
+++ src/parsers.js
@@ -4,8 +4,6 @@ export default (data, format) => {
   switch (format) {
     case 'json':
       return JSON.parse(data);
-    case 'yaml':
-      return yaml.load(data);
     case 'yml':
       return yaml.load(data);
     default:
