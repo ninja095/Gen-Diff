3c10c8d9209d2bd067161530b394f50852b1d9dc
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-25 11:34:57 +0100

    fixed linter stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -10,7 +10,9 @@ const stringify = (node, depth) => {
   if (!_.isObject(node)) {
     return node;
   }
-  const lines = Object.entries(node).map(([key, value]) => `${getIndent(depth +1 )}${doubleSpacer}${key}: ${stringify(value, depth + 1)}`);
+  const lines = Object.entries(node).map(
+    ([key, value]) => `${getIndent(depth + 1)}${doubleSpacer}${key}: ${stringify(value, depth + 1)}`,
+  );
 
   return [
     '{',
@@ -19,27 +21,27 @@ const stringify = (node, depth) => {
   ].join('\n');
 };
 
-  const formatStylish = (data, depth = 1) => {
-    const lines = data.flatMap((diff) => {
-      switch (diff.type) {
-        case 'nested':
-          return `${getIndent(depth)}  ${diff.key}: {\n${formatStylish(diff.children, depth + 1).join('\n')}\n${getIndent(depth)}${doubleSpacer}}`;
-        case 'added':
-          return `${getIndent(depth)}+ ${diff.key}: ${stringify(diff.value2, depth)}`;
-        case 'deleted':
-          return `${getIndent(depth)}- ${diff.key}: ${stringify(diff.value1, depth)}`;
-        case 'unchanged':
-          return `${getIndent(depth)}  ${diff.key}: ${stringify(diff.value1, depth)}`;
-        case 'changed':
-          return [
-            `${getIndent(depth)}- ${diff.key}: ${stringify(diff.value1, depth)}`,
-            `${getIndent(depth)}+ ${diff.key}: ${stringify(diff.value2, depth)}`,
-          ];
-        default:
-          throw new Error(`Unknown type of data: ${diff.type}`);
-      }
-    });
-    return lines
-  };
+const formatStylish = (data, depth = 1) => {
+  const lines = data.flatMap((diff) => {
+    switch (diff.type) {
+      case 'nested':
+        return `${getIndent(depth)}  ${diff.key}: {\n${formatStylish(diff.children, depth + 1).join('\n')}\n${getIndent(depth)}${doubleSpacer}}`;
+      case 'added':
+        return `${getIndent(depth)}+ ${diff.key}: ${stringify(diff.value2, depth)}`;
+      case 'deleted':
+        return `${getIndent(depth)}- ${diff.key}: ${stringify(diff.value1, depth)}`;
+      case 'unchanged':
+        return `${getIndent(depth)}  ${diff.key}: ${stringify(diff.value1, depth)}`;
+      case 'changed':
+        return [
+          `${getIndent(depth)}- ${diff.key}: ${stringify(diff.value1, depth)}`,
+          `${getIndent(depth)}+ ${diff.key}: ${stringify(diff.value2, depth)}`,
+        ];
+      default:
+        throw new Error(`Unknown type of data: ${diff.type}`);
+    }
+  });
+  return lines;
+};
 
 export default (tree) => `{\n${formatStylish(tree).join('\n')}\n}`;
