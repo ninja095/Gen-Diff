4e9d45e04319b29808c12fa60cf3b8b31be55696
Ruslan Moussayev <ninja095@yahoo.com>
2023-03-29 12:55:12 +0200

    fix linter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -19,26 +19,27 @@ const stringify = (node, depth) => {
 const formatStylish = (data, depth = 1) => {
   const lines = data.flatMap(
     ({
-       type, key, value, value1, value2,
-     }) => {
-    switch (type) {
-      case 'nested':
-        return `${getIndent(depth)}  ${key}: {\n${formatStylish(value, depth + 1).join('\n')}\n${getIndent(depth)}${doubleSpacer}}`;
-      case 'added':
-        return `${getIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
-      case 'deleted':
-        return `${getIndent(depth)}- ${key}: ${stringify(value, depth)}`;
-      case 'unchanged':
-        return `${getIndent(depth)}  ${key}: ${stringify(value, depth)}`;
-      case 'changed':
-        return [
-          `${getIndent(depth)}- ${key}: ${stringify(value1, depth)}`,
-          `${getIndent(depth)}+ ${key}: ${stringify(value2, depth)}`,
-        ];
-      default:
-        throw new Error(`Unknown type of data: ${type}`);
-    }
-  });
+      type, key, value, value1, value2,
+    }) => {
+      switch (type) {
+        case 'nested':
+          return `${getIndent(depth)}  ${key}: {\n${formatStylish(value, depth + 1).join('\n')}\n${getIndent(depth)}${doubleSpacer}}`;
+        case 'added':
+          return `${getIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
+        case 'deleted':
+          return `${getIndent(depth)}- ${key}: ${stringify(value, depth)}`;
+        case 'unchanged':
+          return `${getIndent(depth)}  ${key}: ${stringify(value, depth)}`;
+        case 'changed':
+          return [
+            `${getIndent(depth)}- ${key}: ${stringify(value1, depth)}`,
+            `${getIndent(depth)}+ ${key}: ${stringify(value2, depth)}`,
+          ];
+        default:
+          throw new Error(`Unknown type of data: ${type}`);
+      }
+    },
+  );
   return lines;
 };
 
